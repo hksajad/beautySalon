8456b7d49d70a9c4ef51802effd57c46c0339922
sajjad <sajjadhasanikakh06@gmail.com>
2024-09-16 14:02:51 +0330

    add

--- components/template/SocialCaptionHero/Socialcapton.jsx
+++ components/template/SocialCaptionHero/Socialcapton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Socialcapton = () => {
   return (
-    <div className="bg-[#ECD8BD] w-[100%] rounded-[8px]">
+    <div className="bg-[#ecd8bdb6] w-[100%] rounded-[8px]">
       <h1 className="text-[#fff] text-center text-[30px] font-bold pt-4 pb-4 px-3">همیشه ، برای کمی زیبایی  در زندگی خود جا باز کنید </h1>
     </div>
   );
