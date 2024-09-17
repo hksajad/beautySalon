b5540e3c900b044738691aeb4d83330859e8a988
sajjad <sajjadhasanikakh06@gmail.com>
2024-09-17 21:43:18 +0330

    add file

--- app/page.js
+++ app/page.js
@@ -1,21 +1,7 @@
-import Footer from "@/components/module/Footer/Footer";
-import Navbar from "@/components/module/Navbar/Navbar";
-import AboutPage from "@/components/template/AboutPage/AboutPage";
-import FollowUs from "@/components/template/FollowUs/FollowUs";
-import Hero from "@/components/template/HeroPage/Hero";
-import Services from "@/components/template/ServicesPage/Services";
-import Socialcapton from "@/components/template/SocialCaptionHero/Socialcapton";
-
 export default function Home() {
   return (
     <div className="container">
-      <Navbar />
-      <Hero/>
-      <Socialcapton/>
-      <Services/>
-      <AboutPage/>
-      <FollowUs/>
-      <Footer/>
+      
     </div>
   );
 }
