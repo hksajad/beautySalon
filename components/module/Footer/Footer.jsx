b63c3cc481fb41a541ba435ea66c6235f54515d2
sajjad <sajjadhasanikakh06@gmail.com>
2024-09-20 03:09:57 +0330

    add file

--- components/module/Footer/Footer.jsx
+++ components/module/Footer/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
         <div className="container">
             <div className="content_footer flex flex-col items-center justify-center">
                 <Image src='/Img/IconFooter.png' width={100} height={100}/>
-                <p className='text-[#252525] text-bold text-[25px] pt-5'>سالن زیبایی نازگلین </p>
-                <div className="link_footer flex items-center gap-1 pt-10 flex-wrap justify-center">
-                    <h1 className='text-[#252525] text-bold text-[25px] '>طراحی شده توسط</h1>
-                    <Link href="https://developmart.ir/"><Image src="/Img/develop.png" width={200} height={150}/></Link>
+                <p className='text-[#252525] font-bold text-[25px] pt-5'>سالن زیبایی نازگلین </p>
+                <div className="link_footer flex items-center gap-1 pt-10  justify-center ">
+                    <h1 className='text-[#252525] font-bold text-[25px] max-[768px]:text-[17px]'>طراحی شده توسط</h1>
+                    <Link href="https://developmart.ir/" className="mt-3"><Image src="/Img/develop.png" width={200} height={150}/></Link>
                 </div>
             </div>
         </div>
